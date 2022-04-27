2e5593ae45eb0e90bc89ec0c343782c5d7a1a470
tutur1004 <arthur.barthelme78@gmail.com>
2022-04-27 23:43:24 +0200

    API update #2 - Add addPlayerTickets method - Add throws SQLException for getTickets methods

--- src/main/java/fr/milekat/hostmanager/api/API.java
+++ src/main/java/fr/milekat/hostmanager/api/API.java
@@ -4,6 +4,7 @@ import fr.milekat.hostmanager.HostManager;
 import fr.milekat.hostmanager.storage.StorageExecutor;
 import org.bukkit.entity.Player;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 public class API {
@@ -14,7 +15,7 @@ public class API {
      * @param uuid uuid of player
      * @return ticket amount
      */
-    public static Integer getTickets(UUID uuid) {
+    public static Integer getTickets(UUID uuid) throws SQLException {
         return EXECUTOR.getTicket(uuid);
     }
 
@@ -23,7 +24,15 @@ public class API {
      * @param player player
      * @return ticket amount
      */
-    public static Integer getTickets(Player player) {
+    public static Integer getTickets(Player player) throws SQLException {
         return getTickets(player.getUniqueId());
+    }
+
+    /**
+     * Get tickets amount of player
+     * @param player player
+     */
+    public static void addPlayerTickets(Player player, Integer amount) throws SQLException {
+        EXECUTOR.addPlayerTickets(player.getUniqueId(), player.getName(), amount);
     }
 }
