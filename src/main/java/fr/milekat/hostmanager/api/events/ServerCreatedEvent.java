1a24ffba51fe7a15f82d48b67b74b327a2710a45
tutur1004 <arthur.barthelme78@gmail.com>
2022-06-28 00:21:05 +0200

    Simplify events for the API - Remove user from events, user = Instance#host

--- src/main/java/fr/milekat/hostmanager/api/events/ServerCreatedEvent.java
+++ src/main/java/fr/milekat/hostmanager/api/events/ServerCreatedEvent.java
@@ -1,23 +1,15 @@
 package fr.milekat.hostmanager.api.events;
 
 import fr.milekat.hostmanager.api.classes.Instance;
-import fr.milekat.hostmanager.api.classes.User;
 import net.md_5.bungee.api.plugin.Cancellable;
 import net.md_5.bungee.api.plugin.Event;
 
 public class ServerCreatedEvent extends Event implements Cancellable {
     private final Instance instance;
-    private final User user;
     private boolean canceled;
 
     public ServerCreatedEvent(Instance instance) {
         this.instance = instance;
-        this.user = null;
-    }
-
-    public ServerCreatedEvent(Instance instance, User user) {
-        this.instance = instance;
-        this.user = user;
     }
 
     public Instance getInstance() {
--- src/main/java/fr/milekat/hostmanager/api/events/ServerDeletedEvent.java
+++ src/main/java/fr/milekat/hostmanager/api/events/ServerDeletedEvent.java
@@ -1,21 +1,13 @@
 package fr.milekat.hostmanager.api.events;
 
 import fr.milekat.hostmanager.api.classes.Instance;
-import fr.milekat.hostmanager.api.classes.User;
 import net.md_5.bungee.api.plugin.Event;
 
 public class ServerDeletedEvent extends Event {
     private final Instance instance;
-    private final User user;
 
     public ServerDeletedEvent(Instance instance) {
         this.instance = instance;
-        this.user = null;
-    }
-
-    public ServerDeletedEvent(Instance instance, User user) {
-        this.instance = instance;
-        this.user = user;
     }
 
     public Instance getInstance() {
