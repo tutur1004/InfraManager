bc58a441e12f4c5e298a2a4f859882f1588a9a35
tutur1004 <arthur.barthelme78@gmail.com>
2022-06-29 22:55:12 +0200

    Add Pterodactyl Docker template - Dockerfile - Entrypoint (Script run on pterodactyl server start) - Basic minecraft server.properties, with %{VAR}% examples

---
+++ host-template/pterodactyl/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/ash
+
+# Copy game data content to container workdir
+echo "Copy game data content to container workdir"
+cp -r /home/data/* /home/container/
+cd /home/container
+
+# Replace env values in config and .properties files 
+# Replace %{KEY}% with their values
+echo "Replace env values in files"
+export IFS=";"
+d=$'\03'
+for config in $CONFIG; do
+	if echo "$config" | grep -q "="
+	then
+		KEY="$(echo $config | cut -d'=' -f1)"
+		VAL="$(echo $config | cut -d'=' -f2)"
+		find * -type f \( -name "*.yml" -o -name "*.properties" \) -exec sed -i "s${d}%{$KEY}%${d}$VAL${d}g" {} +
+	fi
+done
+unset IFS
+
+# Output Current Java Version
+java -version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo "${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
---
+++ host-template/pterodactyl/pterodactyl.Dockerfile
@@ -0,0 +1,24 @@
+# ----------------------------------
+# Pterodactyl Core Dockerfile
+# Environment: Java
+# Minimum Panel Version: 0.6.0
+# ----------------------------------
+FROM eclipse-temurin:8-jre-alpine
+
+MAINTAINER MileKat
+
+RUN adduser --disabled-password container
+RUN chown -R container:container /home/container
+
+USER container
+ENV USER=container HOME=/home/container
+
+COPY --chown=container:container container /home/data
+RUN chown -R container:container /home/data
+
+WORKDIR /home/container
+
+COPY --chown=container:container entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+CMD ["/bin/ash", "/entrypoint.sh"]
